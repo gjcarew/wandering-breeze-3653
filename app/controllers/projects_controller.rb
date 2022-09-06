bfd349f7d8321f719a913773cd1078a8b7d4decd
Gavin Carew <gjcarew@gmail.com>
2022-09-06 15:42:45 -0700

    Complete extension story 2

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,11 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
   end
+
+  def update
+    project = Project.find(params[:id])
+    contestant = Contestant.find(params[:add_contestant])
+    project.contestants << contestant
+    redirect_to "/projects/#{project.id}"
+  end
 end
--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -12,12 +12,12 @@ RSpec.describe 'Project show page' do
       @upholstery_tux = furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
       @lit_fit = furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
 
-      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
       gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
       kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
       erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
 
-      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+      ContestantProject.create(contestant_id: @jay.id, project_id: news_chic.id)
       ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
       ContestantProject.create(contestant_id: gretchen.id, project_id: @upholstery_tux.id)
       ContestantProject.create(contestant_id: kentaro.id, project_id: @upholstery_tux.id)
@@ -40,6 +40,33 @@ RSpec.describe 'Project show page' do
     it 'I see average years of experience for contestants on the project' do
       visit "/projects/#{@upholstery_tux.id}"
       expect(page).to have_content("Average Contestant Experience: 10.0 years")
+    end
+
+    describe 'adding a contestant to a project' do
+      it 'has a form to add a contestant to a project' do
+        visit "/projects/#{@upholstery_tux.id}"
+        expect(page).to have_field "Add contestant"
+        expect(page).to have_button "Add Contestant to Project"
+      end
+
+      it 'When I fill out the form, it takes me to the project show page where the number of contestants has increased by 1' do
+        visit "/projects/#{@upholstery_tux.id}"
+        expect(page).to have_content('Number of Contestants: 2')
+        fill_in 'Add contestant', with: "#{@jay.id}"
+        click_button "Add Contestant to Project"
+        expect(current_path).to eq("/projects/#{@upholstery_tux.id}")
+        expect(page).to have_content('Number of Contestants: 3')
+      end
+
+      it 'the project is listed in the contestants index page' do
+        visit "/projects/#{@upholstery_tux.id}"
+        fill_in 'Add contestant', with: "#{@jay.id}"
+        click_button "Add Contestant to Project"
+        visit '/contestants'
+        within "#contestant-#{@jay.id}" do
+          expect(page).to have_content(@upholstery_tux.name)
+        end
+      end
     end
   end
 end
